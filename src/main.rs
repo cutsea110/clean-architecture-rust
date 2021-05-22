f4c66b5c422c2072ee6ce9d5a1253759e5c399d7
cutsea110 <cutsea110@gmail.com>
2021-05-23 07:58:57 +0900

    hold multiple daos

--- src/main.rs
+++ src/main.rs
@@ -85,6 +85,8 @@ fn main() {
 
 // ref.) https://keens.github.io/blog/2017/12/01/rustnodi/
 // cake pattern
+
+// User
 pub trait UserDao {
     type FindRequest;
     type FindResponse;
@@ -111,9 +113,102 @@ pub trait HaveUserService {
     fn user_service(&self) -> Self::UserService;
 }
 
+// Group
+
+pub trait GroupDao {
+    type FindRequest;
+    type FindResponse;
+    fn find_group(&self, req: Self::FindRequest) -> Self::FindResponse;
+}
+pub trait HaveGroupDao {
+    type GroupDao: GroupDao;
+    fn group_dao(&self) -> Self::GroupDao;
+}
+
+pub trait GroupService: HaveGroupDao {
+    fn get_group_by_id(
+        &self,
+        req: <<Self as HaveGroupDao>::GroupDao as GroupDao>::FindRequest,
+    ) -> <<Self as HaveGroupDao>::GroupDao as GroupDao>::FindResponse {
+        self.group_dao().find_group(req)
+    }
+}
+
+impl<T: HaveGroupDao> GroupService for T {}
+
+pub trait HaveGroupService {
+    type GroupService: GroupService;
+    fn group_service(&self) -> Self::GroupService;
+}
+
+// Impl
+
+#[derive(Copy, Clone)]
+struct PgConnection;
+
+#[derive(Copy, Clone)]
+struct UserPgDao(PgConnection);
+
+#[derive(Copy, Clone)]
+struct GroupPgDao(PgConnection);
+
+impl UserDao for UserPgDao {
+    type FindRequest = u32;
+    type FindResponse = Option<User>;
+    fn find_user(&self, key: u32) -> Option<User> {
+        match key {
+            1..=100 => None,
+            _ => Some(User {
+                id: key,
+                name: String::from(format!("user name {}", key)),
+            }),
+        }
+    }
+}
+impl HaveUserDao for UserPgDao {
+    type UserDao = Self;
+    fn user_dao(&self) -> Self::UserDao {
+        let con = PgConnection;
+        UserPgDao(con)
+    }
+}
+
+impl GroupDao for GroupPgDao {
+    type FindRequest = u32;
+    type FindResponse = Option<Group>;
+    fn find_group(&self, key: u32) -> Option<Group> {
+        match key {
+            1..=10 => None,
+            _ => Some(Group {
+                id: key,
+                name: String::from(format!("group name {}", key)),
+            }),
+        }
+    }
+}
+impl HaveGroupDao for GroupPgDao {
+    type GroupDao = Self;
+    fn group_dao(&self) -> Self::GroupDao {
+        let con = PgConnection;
+        GroupPgDao(con)
+    }
+}
+
+#[derive(Debug)]
+struct User {
+    pub id: u32,
+    pub name: String,
+}
+
+#[derive(Debug)]
+struct Group {
+    pub id: u32,
+    pub name: String,
+}
 #[derive(Copy, Clone)]
 struct Server {
     pub user_dao: UserPgDao,
+    pub group_dao: GroupPgDao,
 }
 impl HaveUserDao for Server {
     type UserDao = UserPgDao;
@@ -127,43 +222,27 @@ impl HaveUserService for Server {
         *self
     }
 }
-
-#[derive(Copy, Clone)]
-struct PgConnection;
-
-#[derive(Copy, Clone)]
-struct UserPgDao(PgConnection);
-
-impl UserDao for UserPgDao {
-    type FindRequest = u32;
-    type FindResponse = Option<User>;
-    fn find_user(&self, key: u32) -> Option<User> {
-        match key {
-            1..=100 => None,
-            _ => Some(User {
-                id: key,
-                name: String::from(format!("name {}", key)),
-            }),
-        }
+impl HaveGroupDao for Server {
+    type GroupDao = GroupPgDao;
+    fn group_dao(&self) -> GroupPgDao {
+        self.group_dao
     }
 }
-impl HaveUserDao for UserPgDao {
-    type UserDao = Self;
-    fn user_dao(&self) -> Self::UserDao {
-        let con = PgConnection;
-        UserPgDao(con)
+impl HaveGroupService for Server {
+    type GroupService = Self;
+    fn group_service(&self) -> Server {
+        *self
     }
-}
-
-#[derive(Debug)]
-struct User {
-    pub id: u32,
-    pub name: String,
 }
 
 fn main() {
-    let con = PgConnection;
-    let user_dao = UserPgDao(con);
-    let user = user_dao.find_user(101);
+    let server = Server {
+        user_dao: UserPgDao(PgConnection),
+        group_dao: GroupPgDao(PgConnection),
+    };
+
+    let user = server.user_dao.find_user(101);
     println!("{:?}", user);
+    let group = server.group_dao.find_group(21);
+    println!("{:?}", group);
 }
